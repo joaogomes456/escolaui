a615ddf1d86c42b42fc420d526ef5130c9a2e248
JoãoGomes456 <pontesjoao80@hotmail.com>
2022-09-13 08:54:06 -0300

    13/09/2022
    CSS finalizado, ligação de curso com aluno em andamento.

--- src/app/alunos/listaalunos/listaalunos.component.ts
+++ src/app/alunos/listaalunos/listaalunos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Aluno } from 'src/app/core/model';
 
 @Component({
   selector: 'app-listaalunos',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listaalunos.component.scss']
 })
 export class ListaalunosComponent implements OnInit {
+
+  alunos: Aluno[] = [
+    {id: 1, nomealuno: 'Josué'},
+    {id: 2, nomealuno: 'Maria'}
+  ];
 
   constructor() { }
 
